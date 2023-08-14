26016b404e67e7106dfa1b855a06d2c863f9c932
Mustafa sesen <sesenm0506@gmail.com>
2023-08-14 13:57:52 +0300

    first commit

---
+++ src/test/java/HesapMakinesi.java
@@ -0,0 +1,33 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class HesapMakinesi {
+
+    AndroidDriver<MobileElement> driver;
+    AppiumDriver<MobileElement> driver2;
+
+    @Test
+    public void hesapMakinesi() throws MalformedURLException {
+        DesiredCapabilities capabilities =new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+
+
+
+        driver =new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+    }
+
+}
